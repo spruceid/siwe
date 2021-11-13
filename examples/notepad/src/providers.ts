4a2410e9ff7a9c17d82f71ed760557e798cb2bf2
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2021-11-12 22:23:32 -0300

    Save button

--- examples/notepad/src/providers.ts
+++ examples/notepad/src/providers.ts
@@ -12,6 +12,15 @@ const enum Providers {
 const metamask = (window as any).ethereum;
 let walletconnect: WalletConnect;
 
+let metamaskButton: HTMLButtonElement;
+let walletConnectButton: HTMLButtonElement;
+let toggleSize: HTMLButtonElement;
+let closeButton: HTMLButtonElement;
+let disconnectButton: HTMLDivElement;
+let saveButton: HTMLDivElement;
+let notepad: HTMLTextAreaElement;
+let unsaved: HTMLParagraphElement;
+
 /**
  * We need these to remove/add the eventListeners
  */
@@ -121,7 +130,7 @@ const save = async (e?: Mousetrap.ExtendedKeyboardEvent) => {
         credentials: 'include',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text }),
-    });
+    }).then(() => blockSave());
 };
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -144,11 +153,14 @@ document.addEventListener('DOMContentLoaded', () => {
     /**
      * Bellow here are just helper functions to manage app state
      */
-    const metamaskButton = document.getElementById('metamask');
-    const walletConnectButton = document.getElementById('walletconnect');
-    const disconnectButton = document.getElementById('disconnectButton');
-    const toggleSize = document.getElementById('toggleSize');
-
+    metamaskButton = document.getElementById('metamask') as HTMLButtonElement;
+    walletConnectButton = document.getElementById('walletconnect') as HTMLButtonElement;
+    disconnectButton = document.getElementById('disconnectButton') as HTMLDivElement;
+    toggleSize = document.getElementById('toggleSize') as HTMLButtonElement;
+    saveButton = document.getElementById('saveButton') as HTMLDivElement;
+    notepad = document.getElementById('notepad') as HTMLTextAreaElement;
+    closeButton = document.getElementById('closeButton') as HTMLButtonElement;
+    unsaved = document.getElementById('unsaved') as HTMLParagraphElement;
     /**
      * If we don't have metamask installed hide the button.
      */
@@ -160,17 +172,27 @@ document.addEventListener('DOMContentLoaded', () => {
     disconnectButton.addEventListener('click', signOut);
     metamaskButton.addEventListener('click', () => signIn(Providers.METAMASK));
     walletConnectButton.addEventListener('click', () => signIn(Providers.WALLET_CONNECT));
+    saveButton.addEventListener('click', save);
+    notepad.addEventListener('input', enableSave);
 });
+
+const blockSave = () => {
+    saveButton.removeEventListener('click', save);
+    saveButton.setAttribute('disabled', 'true');
+    updateUnsavedChanges('');
+    window.onbeforeunload = null;
+};
+
+const enableSave = () => {
+    saveButton.addEventListener('click', save);
+    saveButton.removeAttribute('disabled');
+    updateUnsavedChanges('- Unsaved Changes');
+    window.onbeforeunload = () => 'Unsaved Changes!';
+};
 
 Mousetrap.bind('mod+s', save);
 
 const connectedState = (text: string, address: string, ens: string) => {
-    const metamaskButton = document.getElementById('metamask');
-    const walletConnectButton = document.getElementById('walletconnect');
-    const closeButton = document.getElementById('closeButton');
-    const disconnectButton = document.getElementById('disconnectButton');
-    const saveButton = document.getElementById('saveButton');
-
     /**
      * Updates fields and buttons
      */
@@ -178,41 +200,33 @@ const connectedState = (text: string, address: string, ens: string) => {
     walletConnectButton.classList.add('hidden');
     closeButton.addEventListener('click', signOut);
     closeButton.removeAttribute('disabled');
-    saveButton.addEventListener('click', save);
-    saveButton.removeAttribute('disabled');
+    saveButton.classList.remove('hidden');
     disconnectButton.classList.remove('hidden');
     if (text) {
         updateNotepad(text);
     }
+    blockSave();
     updateTitle(ens ?? address);
 };
 
 const disconnectedState = () => {
-    const metamaskButton = document.getElementById('metamask');
-    const walletConnectButton = document.getElementById('walletconnect');
-    const closeButton = document.getElementById('closeButton');
-    const disconnectButton = document.getElementById('disconnectButton');
-    const saveButton = document.getElementById('saveButton');
-
     if (typeof metamask !== undefined) {
         metamaskButton.classList.remove('hidden');
     }
     walletConnectButton.classList.remove('hidden');
     closeButton.removeEventListener('click', signOut);
     closeButton.setAttribute('disabled', 'disabled');
-    saveButton.removeEventListener('click', save);
-    saveButton.setAttribute('disabled', 'disabled');
+    saveButton.classList.add('hidden');
     disconnectButton.classList.add('hidden');
 };
 
 const updateTitle = (text: string) => (document.getElementById('title').innerText = text);
 
-const updateNotepad = (text: string) =>
-    ((document.getElementById('notepad') as HTMLTextAreaElement).value = text);
+const updateUnsavedChanges = (text: string) => (unsaved.innerText = text);
+
+const updateNotepad = (text: string) => (notepad.value = text);
 
 const maximize = () => {
-    const toggleSize = document.getElementById('toggleSize');
-    const notepad = document.getElementById('notepad') as HTMLTextAreaElement;
     toggleSize.removeEventListener('click', maximize);
     toggleSize.addEventListener('click', restore);
     toggleSize.ariaLabel = 'Restore';
@@ -221,8 +235,6 @@ const maximize = () => {
 };
 
 const restore = () => {
-    const toggleSize = document.getElementById('toggleSize');
-    const notepad = document.getElementById('notepad') as HTMLTextAreaElement;
     toggleSize.removeEventListener('click', restore);
     toggleSize.addEventListener('click', maximize);
     toggleSize.ariaLabel = 'Maximize';
