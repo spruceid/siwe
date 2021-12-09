d915697b4dad3ba6151477ef0af422b16b9f0196
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2021-12-09 16:10:27 -0300

    Moves begin and and to message, moves endlines to beggining of sentence

--- lib/regex.ts
+++ lib/regex.ts
@@ -1,21 +1,21 @@
 const DOMAIN =
-	'^(?<domain>([^?#]*)) wants you to sign in with your Ethereum account:\\n';
-const ADDRESS = '(?<address>0x[a-zA-Z0-9]{40})\\n\\n';
-const STATEMENT = '((?<statement>[^\\n]+)\\n)?\\n';
+	'(?<domain>([^?#]*)) wants you to sign in with your Ethereum account:';
+const ADDRESS = '\\n(?<address>0x[a-zA-Z0-9]{40})\\n\\n';
+const STATEMENT = '((?<statement>[^\\n]+)\\n)?';
 const URI = '(([^:?#]+):)?(([^?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))';
-const URI_LINE = `URI: (?<uri>${URI}?)\\n`;
-const VERSION = 'Version: (?<version>1)\\n';
-const CHAIN_ID = 'Chain ID: (?<chainId>[0-9]+)\\n';
-const NONCE = 'Nonce: (?<nonce>[a-zA-Z0-9]{8,})\\n';
+const URI_LINE = `\\nURI: (?<uri>${URI}?)`;
+const VERSION = '\\nVersion: (?<version>1)';
+const CHAIN_ID = '\\nChain ID: (?<chainId>[0-9]+)';
+const NONCE = '\\nNonce: (?<nonce>[a-zA-Z0-9]{8,})';
 const DATETIME = `([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\.[0-9]+)?(([Zz])|([\+|\-]([01][0-9]|2[0-3]):[0-5][0-9]))`;
-const ISSUED_AT = `Issued At: (?<issuedAt>${DATETIME})`;
+const ISSUED_AT = `\\nIssued At: (?<issuedAt>${DATETIME})`;
 const EXPIRATION_TIME = `(\\nExpiration Time: (?<expirationTime>${DATETIME}))?`;
 const NOT_BEFORE = `(\\nNot Before: (?<notBefore>${DATETIME}))?`;
 const REQUEST_ID =
 	"(\\nRequest ID: (?<requestId>[-._~!$&'()*+,;=:@%a-zA-Z0-9]*))?";
-const RESOURCES = `(\\nResources:(?<resources>(\\n- ${URI}?)+))?$`;
+const RESOURCES = `(\\nResources:(?<resources>(\\n- ${URI}?)+))?`;
 
-const MESSAGE = `${DOMAIN}${ADDRESS}${STATEMENT}${URI_LINE}${VERSION}${CHAIN_ID}${NONCE}${ISSUED_AT}${EXPIRATION_TIME}${NOT_BEFORE}${REQUEST_ID}${RESOURCES}`;
+const MESSAGE = `^${DOMAIN}${ADDRESS}${STATEMENT}${URI_LINE}${VERSION}${CHAIN_ID}${NONCE}${ISSUED_AT}${EXPIRATION_TIME}${NOT_BEFORE}${REQUEST_ID}${RESOURCES}$`;
 
 export class ParsedMessage {
 	domain: string;
