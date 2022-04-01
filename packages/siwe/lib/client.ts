fab78c17857089bdce44fa40547a9c6692273dbc
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2022-04-01 07:25:36 -0300

    Fix smart contract wallet function (#63)
    * Fix smart contract wallet function
    
    * Catches error for signatures longer then 65 bytes

--- packages/siwe/lib/client.ts
+++ packages/siwe/lib/client.ts
@@ -249,22 +249,27 @@ export class SiweMessage {
 					);
 				}
 
-				const addr = ethers.utils.verifyMessage(message, signature);
+				let addr;
+				try {
+					addr = ethers.utils.verifyMessage(message, signature);
 
-				if (addr !== this.address) {
-					try {
-						//EIP-1271
-						const isValidSignature =
-							await checkContractWalletSignature(this, provider);
-						if (!isValidSignature) {
-							throw new Error(
-								`${ErrorTypes.INVALID_SIGNATURE}: ${addr} !== ${this.address}`
-							);
+				} catch (_) { } finally {
+					if (addr !== this.address) {
+						try {
+							//EIP-1271
+							const isValidSignature =
+								await checkContractWalletSignature(this, signature, provider);
+							if (!isValidSignature) {
+								throw new Error(
+									`${ErrorTypes.INVALID_SIGNATURE}: ${addr} !== ${this.address}`
+								);
+							}
+						} catch (e) {
+							throw e;
 						}
-					} catch (e) {
-						throw e;
 					}
 				}
+
 				const parsedMessage = new SiweMessage(message);
 
 				if (parsedMessage.expirationTime) {
@@ -297,6 +302,7 @@ export class SiweMessage {
  */
 export const checkContractWalletSignature = async (
 	message: SiweMessage,
+	signature: string,
 	provider?: any
 ): Promise<boolean> => {
 	if (!provider) {
@@ -311,7 +317,7 @@ export const checkContractWalletSignature = async (
 		const hashMessage = utils.hashMessage(message.signMessage());
 		return await walletContract.isValidSignature(
 			hashMessage,
-			message.signature
+			signature,
 		);
 	} catch (e) {
 		throw e;
