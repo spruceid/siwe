70d424013c4f6f93c2377cae48f19916dc34c51e
hzhu <hz@henryzhu.me>
2022-08-28 16:02:48 -0700

    Refactor parser utility to use keccak hash from ethers

--- packages/siwe-parser/lib/utils.ts
+++ packages/siwe-parser/lib/utils.ts
@@ -1,12 +1,13 @@
+import { id } from '@ethersproject/hash';
+
 /**
  * This method is supposed to check if an address is conforming to EIP-55.
  * @param address Address to be checked if conforms with EIP-55.
  * @returns Either the return is or not in the EIP-55 format.
  */
 export const isEIP55Address = (address: string) => {
-    const createKeccakHash = require('keccak')
     const lowerAddress = `${address}`.toLowerCase().replace('0x', '')
-    var hash = createKeccakHash('keccak256').update(lowerAddress).digest('hex')
+    var hash = id(lowerAddress).replace('0x', '')
     var ret = '0x'
 
     for (var i = 0; i < lowerAddress.length; i++) {
