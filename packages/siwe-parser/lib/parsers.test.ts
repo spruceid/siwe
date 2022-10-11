900676457be7911aa99b9028f668953f6e9b5b78
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2022-10-11 16:26:30 -0300

    Remove Regex Parser (#120)
    * Removed regex parser
    
    * remove regex from client
    
    Co-authored-by: Samuel Gbafa <skgbafa@gmail.com>

--- packages/siwe-parser/lib/parsers.test.ts
+++ packages/siwe-parser/lib/parsers.test.ts
@@ -36,38 +36,3 @@ describe("Successfully fails with ABNF Client", () => {
 		}
 	);
 });
-
-describe("Successfully parses with RegExp Client", () => {
-	let ParsedMessage;
-	beforeEach(
-		async () => (ParsedMessage = (await import("./regex")).ParsedMessage)
-	);
-
-	test.concurrent.each(Object.entries(parsingPositive))(
-		"Parses message successfully: %s",
-		(test_name, test) => {
-			const parsedMessage = new ParsedMessage(test.message);
-			for (const [field, value] of Object.entries(test.fields)) {
-				if (typeof value === "object") {
-					expect(parsedMessage[field]).toStrictEqual(value);
-				} else {
-					expect(parsedMessage[field]).toBe(value);
-				}
-			}
-		}
-	);
-});
-
-describe("Successfully fails with RegExp Client", () => {
-	let ParsedMessage;
-	beforeEach(
-		async () => (ParsedMessage = (await import(`./regex`)).ParsedMessage)
-	);
-
-	test.concurrent.each(Object.entries(parsingNegative))(
-		"Fails to parse message: %s",
-		(test_name, test) => {
-			expect(() => new ParsedMessage(test)).toThrow();
-		}
-	);
-});
