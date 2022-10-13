e43c6f0c2d6949ec921f1c1b10bc81ca7804e457
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2022-10-13 11:14:22 -0300

    Adds extra checks for dates (#127)
    * Adds extra checks for dates
    
    * Invalid Date check
    
    * Move ISO8601 regex out of function
    
    * Remove unused variable

--- packages/siwe/lib/utils.ts
+++ packages/siwe/lib/utils.ts
@@ -4,6 +4,7 @@ import type { SiweMessage } from './client';
 
 const EIP1271_ABI = ["function isValidSignature(bytes32 _message, bytes _signature) public view returns (bytes4)"];
 const EIP1271_MAGICVALUE = "0x1626ba7e";
+const ISO8601 = /^(?<date>[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]))[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(.[0-9]+)?(([Zz])|([+|-]([01][0-9]|2[0-3]):[0-5][0-9]))$/;
 
 /**
  * This method calls the EIP-1271 method for Smart Contract wallets
@@ -49,7 +50,32 @@ export const generateNonce = (): string => {
   return nonce;
 };
 
-export const checkInvalidKeys = <T>(obj: T, keys: Array<keyof T>) : Array<keyof T> => {
+/**
+ * This method matches the given date string against the ISO-8601 regex and also
+ * performs checks if it's a valid date.
+ * @param inputDate any string to be validated against ISO-8601
+ * @returns boolean indicating if the providade date is valid and conformant to ISO-8601
+ */
+export const isValidISO8601Date = (inputDate: string): boolean => {
+  /* Split groups and make sure inputDate is in ISO8601 format */
+  const inputMatch = ISO8601.exec(inputDate);
+
+  /* if inputMatch is null the date is not ISO-8601 */
+  if (!inputDate) {
+    return false;
+  }
+
+  /* Creates a date object with input date to parse for invalid days e.g. Feb, 30 -> Mar, 01 */
+  const inputDateParsed = new Date(inputMatch.groups.date).toISOString();
+
+  /* Get groups from new parsed date to compare with the original input */
+  const parsedInputMatch = ISO8601.exec(inputDateParsed);
+
+  /* Compare remaining fields */
+  return inputMatch.groups.date === parsedInputMatch.groups.date;
+}
+
+export const checkInvalidKeys = <T>(obj: T, keys: Array<keyof T>): Array<keyof T> => {
   const invalidKeys: Array<keyof T> = [];
   Object.keys(obj).forEach(key => {
     if (!keys.includes(key as keyof T)) {
