4ab9a61526d2fcbc877dd961c2fd78a8d4a7b29f
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-04-11 17:37:00 +0700

    fix : linting

--- packages/siwe/lib/ethersCompat.ts
+++ packages/siwe/lib/ethersCompat.ts
@@ -5,26 +5,26 @@ type Ethers6BigNumberish = string | number | bigint;
 type Ethers6SignatureLike =
   | string
   | {
-      r: string;
-      s: string;
-      v: Ethers6BigNumberish;
-      yParity?: 0 | 1;
-      yParityAndS?: string;
-    }
+    r: string;
+    s: string;
+    v: Ethers6BigNumberish;
+    yParity?: 0 | 1;
+    yParityAndS?: string;
+  }
   | {
-      r: string;
-      yParityAndS: string;
-      yParity?: 0 | 1;
-      s?: string;
-      v?: number;
-    }
+    r: string;
+    yParityAndS: string;
+    yParity?: 0 | 1;
+    s?: string;
+    v?: number;
+  }
   | {
-      r: string;
-      s: string;
-      yParity: 0 | 1;
-      v?: Ethers6BigNumberish;
-      yParityAndS?: string;
-    };
+    r: string;
+    s: string;
+    yParity: 0 | 1;
+    v?: Ethers6BigNumberish;
+    yParityAndS?: string;
+  };
 
 let ethersVerifyMessage = null;
 let ethersHashMessage = null;
